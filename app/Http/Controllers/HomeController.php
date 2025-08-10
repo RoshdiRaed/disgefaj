ef98c50304bc83be55ca968360c094afffc60e0f
roshdiraed <roshdi013@gmail.com>
2025-08-10 16:29:22 +0300

    v1.1.1

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,14 +3,25 @@
 namespace App\Http\Controllers;
 
 use App\Models\Skill;
-use App\Models\Project; // أضف هذا
+use App\Models\Project;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        $skills = Skill::all();
-        $projects = Project::all(); // جلب المشاريع هنا
+        $skills = $this->getSkills();
+        $projects = $this->getProjects();
+
         return view('welcome', compact('skills', 'projects'));
+    }
+
+    protected function getSkills()
+    {
+        return Skill::all();
+    }
+
+    protected function getProjects()
+    {
+        return Project::all();
     }
 }
